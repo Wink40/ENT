8e1bc7c81d2fc19e81be9bf7a664354f354c5de8
Wink40 <syed.study19@gmail.com>
2024-06-02 18:11:01 +0100

    Update ent_on_call.py

--- ent_on_call.py
+++ ent_on_call.py
@@ -7,7 +7,7 @@ with st.container():
 
     with col1:
         # Display the image on the left
-        st.image("left_top.jpg", width=200, height=150)
+        st.image("left_top.jpg", width=200)
 
     with col2:
         # Add a spacer column
@@ -15,7 +15,7 @@ with st.container():
 
     with col3:
         # Display the image on the right
-        st.image("right_top.jpeg", width=200, height=150)
+        st.image("right_top.jpeg", width=200)
 
 # Add a dividing line
 st.markdown("------------")
